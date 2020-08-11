f783c1f3a8624c9f400ef8c3c030a244a41fac53
Tom Maravilla <tlmaravilla@gmail.com>
2020-08-10 21:43:46 -0600

    added configuration and logging

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Core;
 
 namespace PowerfulConsole
 {
+
+    /*
+     * Main goals for this console app:
+     * - DI
+     * - Serilog for logging
+     * - Settings
+     */
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            // Adding Logging
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+                    optional: true)
+                .AddEnvironmentVariables();
         }
     }
 }
